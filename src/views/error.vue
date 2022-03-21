4fbfe344725b1644bb886ca838666788e90cbd6c
jianfengtheboy <jianfengtheboy@163.com>
2022-03-21 15:16:40 +0800

    update

--- src/views/error.vue
+++ src/views/error.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="error-page">
-    <van-empty image="error" :description="`出错了，${$route.params.code || '404'}!`"></van-empty>
-    <van-button
-      color="linear-gradient(to right, #ff6034, #ee0a24)"
-      size="small"
-      icon="arrow-left"
-      @click="backToHome"
-    >
-      返回首页
-    </van-button>
+    <van-empty image="error" :description="`出错了，${$route.params.code || '404'}!`">
+      <van-button
+        color="linear-gradient(to right, #ff6034, #ee0a24)"
+        size="small"
+        icon="arrow-left"
+        @click="backToHome"
+      >
+        返回首页
+      </van-button>
+    </van-empty>
   </div>
 </template>
 
